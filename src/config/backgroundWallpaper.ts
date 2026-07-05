7c60adad08f5860a02aaa7cf26082b8f978284fd
愁绵绵 <naixiounijiang@gmail.com>
2026-07-05 20:18:01 +0800

    这是我的博客

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -43,22 +43,24 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 		// 桌面背景图片（支持单张或多张随机）
 		// desktop: "assets/images/DesktopWallpaper/d1.avif",
 		desktop: [
-			"assets/images/DesktopWallpaper/d1.avif",
-			"assets/images/DesktopWallpaper/d2.avif",
-			"assets/images/DesktopWallpaper/d3.avif",
-			"assets/images/DesktopWallpaper/d4.avif",
-			"assets/images/DesktopWallpaper/d5.avif",
-			"assets/images/DesktopWallpaper/d6.avif",
+			"assets/images/DesktopWallpaper/92665252_p0.png",
+			"assets/images/DesktopWallpaper/bg.jpg",
+			"assets/images/DesktopWallpaper/crimson-warrior-asuka-langley-soryu-in-neon-genesis-evangelion-uu.jpg",
+			"assets/images/DesktopWallpaper/nino-nakano-5-toubun-no-hanayome-yn.jpg",
+			"assets/images/DesktopWallpaper/zi.png",
+			"assets/images/DesktopWallpaper/utattemita-album-covers-clean-ver-by-v0-p3d3w9znofob1 (2).png",
+
 		],
 		// 移动背景图片（支持单张或多张随机）
 		// mobile: "assets/images/MobileWallpaper/m1.avif",
 		mobile: [
-			"assets/images/MobileWallpaper/m1.avif",
-			"assets/images/MobileWallpaper/m2.avif",
-			"assets/images/MobileWallpaper/m3.avif",
-			"assets/images/MobileWallpaper/m4.avif",
-			"assets/images/MobileWallpaper/m5.avif",
-			"assets/images/MobileWallpaper/m6.avif",
+			"assets/images/MobileWallpaper/1.jpg",
+			"assets/images/MobileWallpaper/2.jpg",
+			"assets/images/MobileWallpaper/3.jpg",
+			"assets/images/MobileWallpaper/4.jpg",
+			"assets/images/MobileWallpaper/5.jpg",
+			"assets/images/MobileWallpaper/6.jpg",
+	
 		],
 		// 背景视频播放地址
 		// 支持单个视频路径（字符串）或多个视频循环（数组）
@@ -81,7 +83,7 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 			// 是否允许用户通过控制面板切换横幅标题显示
 			switchable: true,
 			// 主页横幅主标题
-			title: "Lovely firefly!",
+			title: "Charming Charmian",
 			// 主页横幅主标题字体大小
 			titleSize: "3.8rem",
 			// 主页横幅副标题
